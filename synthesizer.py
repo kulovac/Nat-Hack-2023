8680dff4afc14e29515c293d128570c087db7a33
TheVinayJ <150850618+TheVinayJ@users.noreply.github.com>
2023-11-17 19:30:06 -0700

    Update synthesizer.py
    Replaced with wavetable-type synthesis so it's not just boring sine waves

--- synthesizer.py
+++ synthesizer.py
@@ -1,39 +1,44 @@
-import time
+# Single-Voice Synthesizer
+
 import numpy as np
-import pyaudio
+import scipy.io.wavfile as wav
 
-def mapFreq(Flow, Fhigh, mapLow, mapHigh, freq):
-    d = Fhigh / Flow
-    a = (mapLow**d/mapHigh)**(1/(d-1))
-    c = (mapLow / a)**(1/Flow)
+def synthesize(time: int, sampleRate: int, gain: int, frequency: int, wavetable : np.ndarray):
+    #Main Function
+
+    time = 5
+    waveform = np.sin
+
+    wavetableLength = 64
+    wavetable = np.zeros((wavetableLength,))
+
+    for i in range(wavetableLength):
+        wavetable[i] = waveform(2* np.pi * i / wavetableLength)
+
+    output = np.zeros((time * sampleRate,))
+
+    index = 0
+    indexIncrement = frequency * wavetableLength / sampleRate
     
-    return (a, c, a * c**freq)
+    for n in range(output.shape[0]):
+        output[n] = wavetable[int(np.floor(index))]
+        index += indexIncrement
+        index %= wavetableLength
+
+    volume = 10 ** (-gain / 20)
+    output *= volume
+
+    return output
+
+def main():
+    sampleRate = 44100
+    freq = 440
+    time = 3
+    volumeReduction = 20
+    numpyArray = [0] #Placeholder for now
+    output = synthesize(time, sampleRate, volumeReduction, freq, numpyArray)
+    wav.write('Audio.wav', sampleRate, output.astype(np.float32))
 
 
-volume = 0.5  # range [0.0, 1.0]
-fs = 44100  # sampling rate, Hz, must be integer
-duration = 5.0  # in seconds, may be float
-f = 440.0  # sine frequency, Hz, may be float
-
-p = pyaudio.PyAudio()
-# generate samples, note conversion to float32 array
-samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
-
-# per @yahweh comment explicitly convert to bytes sequence
-output_bytes = (volume * samples).tobytes()
-
-# for paFloat32 sample values must be in range [-1.0, 1.0]
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
-
-# play. May repeat with different volume values (if done interactively)
-start_time = time.time()
-stream.write(output_bytes)
-print("Played sound for {:.2f} seconds".format(time.time() - start_time))
-
-stream.stop_stream()
-stream.close()
-
-p.terminate()
+if __name__ == "__main__":
+    main()
