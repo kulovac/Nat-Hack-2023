d9dcbbfaf54e6a958fc71444c5264c95824524f4
TheVinayJ <150850618+TheVinayJ@users.noreply.github.com>
2023-11-18 14:39:38 -0700

    I ADDED KEYS LIKE A PIANO :DDDD

--- synthesizer.py
+++ synthesizer.py
@@ -24,17 +24,28 @@ map2 = [10, 20, 120, 400]
 map3 = [30, 40, 400, 1000]
 map4 = [60, 70, 1000, 5000]
 # C Major
-notesList = [41.2, 43.65, 49.0, 55.0, 61.74, 65.41, 73.42, 82.41, 87.31, 98.0, 110.0, 123.47, 130.81, 146.83,
-              164.81, 174.61, 196.0, 220.0, 246.94, 261.63, 293.66, 329.63, 349.23, 392.0, 440.0, 493.88, 523.25,
-                587.33, 659.25, 698.46, 783.99, 880.0, 987.77, 1046.5, 1174.66, 1318.51, 1396.91, 1567.98, 1760.0,
-                  1975.53, 2093.0, 2349.32, 2637.02, 2793.83, 3135.96, 3520.0, 3951.07, 4186.01, 4698.63]
+notesList = {'A': [55.0, 110.0, 220.0, 440.0, 880.0, 1760.0, 3520.0], 
+             'B': [61.74, 123.47, 246.94, 493.88, 987.77, 1975.53, 3951.07], 
+             'C': [65.41, 130.81, 261.63, 523.25, 1046.5, 2093.0, 4186.01], 
+             'D': [73.42, 146.83, 293.66, 587.33, 1174.66, 2349.32, 4698.63], 
+             'E': [41.2, 82.41, 164.81, 329.63, 659.25, 1318.51, 2637.02], 
+             'F': [43.65, 87.31, 174.61, 349.23, 698.46, 1396.91, 2793.83], 
+             'G': [49.0, 98.0, 196.0, 392.0, 783.99, 1567.98, 3135.96], 
+             'A#/Bb': [58.27, 116.54, 233.08, 466.16, 932.33, 1864.66, 3729.31], 
+             'F#/Gb': [46.25, 92.5, 185.0, 369.99, 739.99, 1479.98, 2959.96], 
+             'C#/Db': [69.3, 138.59, 277.18, 554.37, 1108.73, 2217.46, 4434.92], 
+             'D#/Eb': [77.78, 155.56, 311.13, 622.25, 1244.51, 2489.02, 4978.03], 
+             'G#/Ab': [51.91, 103.83, 207.65, 415.3, 830.61, 1661.22, 3322.44]}
 m1= mapFreqBounds(map1)
 m2 = mapFreqBounds(map2)
 m3 = mapFreqBounds(map3)
 m4 = mapFreqBounds(map4)
 prevOutput = np.empty(0)
 
-def chooseMap(freq):
+def chooseMap(freq, keysPressed):
+    notes = []
+    for key in keysPressed:
+        notes += notesList[key]
     if map1[0] <= freq and map1[1] >= freq:
         preFreq = m1[0] * m1[1] ** freq
     elif map2[0] <= freq and map2[1] >= freq:
@@ -43,7 +54,7 @@ def chooseMap(freq):
         preFreq = m3[0] * m3[1] ** freq
     elif map4[0] <= freq and map4[1] >= freq:
         preFreq = m4[0] * m4[1] ** freq
-    return(search(preFreq, notesList))
+    return(search(preFreq, notes))
 
 def search(freq, notes):
     if freq < notes[0]:
@@ -114,7 +125,7 @@ def synthesize(time: float, sampleRate: int, gain: int, frequency: int, wavetabl
 
     return output
 
-def main2(inFreqList1, inFreqList2, inFreqList3, inFreqList4, waveTable1, waveTable2, waveTable3, waveTable4, filename, fullOutput):
+def main2(inFreqList1, inFreqList2, inFreqList3, inFreqList4, waveTable1, waveTable2, waveTable3, waveTable4, filename, fullOutput, keysPressed):
     time = 1
     volumeReduction = 20
     for freq in inFreqList1:
@@ -144,6 +155,7 @@ def main2(inFreqList1, inFreqList2, inFreqList3, inFreqList4, waveTable1, waveTa
 def main(filename, fullOutput):
     global prevOutput
     prevOutput = np.empty(0)
+    keysPressed = ['D', 'A', 'F#/Gb', 'C#/Db']
     for i in range(10):
         a = random.randint(4,8)
         a1 = random.randint(4,8)
@@ -158,7 +170,7 @@ def main(filename, fullOutput):
         d1 = random.randint(60,70)
         d2 = random.randint(60,70)
         l1, l2, l3, l4 = Muse_S_reader.process_waveform(i/4,(i+1)/4)
-        fullOutput = main2([a, a1, a2], [b, b1, b2], [c, c1, c2], [d, d1, d2], l1, l2, l3, l4, filename, fullOutput)
+        fullOutput = main2([a, a1, a2], [b, b1, b2], [c, c1, c2], [d, d1, d2], l1, l2, l3, l4, filename, fullOutput, keysPressed)
     return(fullOutput)
 
 if __name__ == "__main__":
