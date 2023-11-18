a1d1a69a5c515cdc3ca59eb7b74f2b3c7e3bb265
TheVinayJ <150850618+TheVinayJ@users.noreply.github.com>
2023-11-18 13:07:53 -0700

    Update synthesizer.py

--- synthesizer.py
+++ synthesizer.py
@@ -23,7 +23,11 @@ map1 = [4, 8 , 40, 120]
 map2 = [10, 20, 120, 400]
 map3 = [30, 40, 400, 1000]
 map4 = [60, 70, 1000, 5000]
-notes = [41.2, 43.65, 46.25, 49.0, 51.91, 55.0, 58.27, 61.74, 65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98.0, 103.83, 110.0, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.0, 196.0, 207.65, 220.0, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.0, 415.3, 440.0, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880.0, 932.33, 987.77, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.0, 1864.66, 1975.53, 2093.0, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.0, 3729.31, 3951.07, 4186.01, 4434.92, 4698.63, 4978.03]
+# C Major
+notesList = [41.2, 43.65, 49.0, 55.0, 61.74, 65.41, 73.42, 82.41, 87.31, 98.0, 110.0, 123.47, 130.81, 146.83,
+              164.81, 174.61, 196.0, 220.0, 246.94, 261.63, 293.66, 329.63, 349.23, 392.0, 440.0, 493.88, 523.25,
+                587.33, 659.25, 698.46, 783.99, 880.0, 987.77, 1046.5, 1174.66, 1318.51, 1396.91, 1567.98, 1760.0,
+                  1975.53, 2093.0, 2349.32, 2637.02, 2793.83, 3135.96, 3520.0, 3951.07, 4186.01, 4698.63]
 m1= mapFreqBounds(map1)
 m2 = mapFreqBounds(map2)
 m3 = mapFreqBounds(map3)
@@ -39,9 +43,28 @@ def chooseMap(freq):
         preFreq = m3[0] * m3[1] ** freq
     elif map4[0] <= freq and map4[1] >= freq:
         preFreq = m4[0] * m4[1] ** freq
-    return(search(preFreq))
+    return(search(preFreq, notesList))
 
-def search(freq):
+def search(freq, notes):
+    if freq < notes[0]:
+        return notes[0]
+    if freq > notes[-1]:
+        return notes[-1]
+    
+    if freq in notes:
+        return freq
+    else:
+        if len(notes) == 2:
+            if (freq - notes[0]) < (freq - notes[1]):
+                return notes[0]
+            else:
+                return notes[1]
+        else:
+            middle = len(notes) // 2
+            if freq < notes[middle]:
+                return search(freq, notes[0:middle])
+            else:
+                return search(freq, notes[middle:])
     
 def synthesize(time: float, sampleRate: int, gain: int, frequency: int, wavetable : np.ndarray):
     #Main Function
