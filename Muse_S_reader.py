7cdcc3953c5d01ecfde7a690c65af9ebed49bd13
Amar Kulovac <kulovac@ualberta.ca>
2023-11-17 21:02:31 -0700

    filtered frequencies between 0-50Hz

--- Muse_S_reader.py
+++ Muse_S_reader.py
@@ -5,123 +5,91 @@ frequency domain while filtering out the 60Hz
 powerline frequency
 """
 
-import argparse
-import logging
+import numpy as np
+import matplotlib.pyplot as plt
 
-import pyqtgraph as pg
-from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
-from brainflow.data_filter import DataFilter, FilterTypes, DetrendOperations
-from pyqtgraph.Qt import QtGui, QtCore
+SAMPLES_PER_SECOND = 256  # 256Hz sample rate
 
-
-class Graph:
-    def __init__(self, board_shim):
-        self.board_id = board_shim.get_board_id()
-        self.board_shim = board_shim
-        self.exg_channels = BoardShim.get_exg_channels(self.board_id)
-        self.sampling_rate = BoardShim.get_sampling_rate(self.board_id)
-        self.update_speed_ms = 50
-        self.window_size = 4
-        self.num_points = self.window_size * self.sampling_rate
-
-        self.app = QtGui.QApplication([])
-        self.win = pg.GraphicsWindow(title='BrainFlow Plot', size=(800, 600))
-
-        self._init_timeseries()
-
-        timer = QtCore.QTimer()
-        timer.timeout.connect(self.update)
-        timer.start(self.update_speed_ms)
-        QtGui.QApplication.instance().exec_()
-
-    def _init_timeseries(self):
-        self.plots = list()
-        self.curves = list()
-        for i in range(len(self.exg_channels)):
-            p = self.win.addPlot(row=i, col=0)
-            p.showAxis('left', False)
-            p.setMenuEnabled('left', False)
-            p.showAxis('bottom', False)
-            p.setMenuEnabled('bottom', False)
-            if i == 0:
-                p.setTitle('TimeSeries Plot')
-            self.plots.append(p)
-            curve = p.plot()
-            self.curves.append(curve)
-
-    def update(self):
-        data = self.board_shim.get_current_board_data(self.num_points)
-        for count, channel in enumerate(self.exg_channels):
-            # plot timeseries
-            DataFilter.detrend(data[channel], DetrendOperations.CONSTANT.value)
-            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 3.0, 45.0, 2,
-                                        FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
-            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 48.0, 52.0, 2,
-                                        FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
-            DataFilter.perform_bandstop(data[channel], self.sampling_rate, 58.0, 62.0, 2,
-                                        FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
-            self.curves[count].setData(data[channel].tolist())
-
-        self.app.processEvents()
+POWERLINE_FREQ = 60
+TOLERANCE = 2
 
 
-def main():
-    BoardShim.enable_dev_board_logger()
-    logging.basicConfig(level=logging.DEBUG)
+def FFT_Filter(signal):
+    """
+    Computes the FFT of the signal
+    and removes the POWERLINE_FREQ
+    TODO: 
+    """
+    global SAMPLES_PER_SECOND
+    global POWERLINE_FREQ
+    global TOLERANCE
 
-    parser = argparse.ArgumentParser()
-    # use docs to check which parameters are required for specific board, e.g. for Cyton - set serial port
-    parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
-                        default=0)
-    parser.add_argument('--ip-port', type=int,
-                        help='ip port', required=False, default=0)
-    parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
-                        default=0)
-    parser.add_argument('--ip-address', type=str,
-                        help='ip address', required=False, default='')
-    parser.add_argument('--serial-port', type=str,
-                        help='serial port', required=False, default='')
-    parser.add_argument('--mac-address', type=str,
-                        help='mac address', required=False, default='')
-    parser.add_argument('--other-info', type=str,
-                        help='other info', required=False, default='')
-    parser.add_argument('--streamer-params', type=str,
-                        help='streamer params', required=False, default='')
-    parser.add_argument('--serial-number', type=str,
-                        help='serial number', required=False, default='')
-    parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
-                        required=False, default=BoardIds.SYNTHETIC_BOARD)
-    parser.add_argument('--file', type=str, help='file',
-                        required=False, default='')
-    parser.add_argument('--master-board', type=int, help='master board id for streaming and playback boards',
-                        required=False, default=BoardIds.NO_BOARD)
-    args = parser.parse_args()
+    fft_signal = np.fft.fft(signal)
+    freq = np.fft.fftfreq(len(signal), 1 / SAMPLES_PER_SECOND)
 
-    params = BrainFlowInputParams()
-    params.ip_port = args.ip_port
-    params.serial_port = args.serial_port
-    params.mac_address = args.mac_address
-    params.other_info = args.other_info
-    params.serial_number = args.serial_number
-    params.ip_address = args.ip_address
-    params.ip_protocol = args.ip_protocol
-    params.timeout = args.timeout
-    params.file = args.file
-    params.master_board = args.master_board
+    fft_signal = fft_signal[1:len(signal)//2]
+    freq = freq[1:len(freq)//2]
 
-    board_shim = BoardShim(args.board_id, params)
-    try:
-        board_shim.prepare_session()
-        board_shim.start_stream(450000, args.streamer_params)
-        Graph(board_shim)
-    except BaseException:
-        logging.warning('Exception', exc_info=True)
-    finally:
-        logging.info('End')
-        if board_shim.is_prepared():
-            logging.info('Releasing session')
-            board_shim.release_session()
+    # cutoff at 50Hz as beta waves do not go past this value
+    max_freq = np.abs(freq - 50).argmin()
+
+    freq = freq[1:max_freq]
+    fft_signal = fft_signal[1:max_freq]
+
+    return (fft_signal, freq)
 
 
-if __name__ == '__main__':
-    main()
+def read(filename, start, end):
+    """
+    reads all values from a start time and
+    end time where time is dictated by the
+    sample rate
+    """
+    global SAMPLES_PER_SECOND
+    wave1 = np.array([])
+    wave2 = np.array([])
+    wave3 = np.array([])
+    wave4 = np.array([])
+
+    start_index = int(start * SAMPLES_PER_SECOND)
+    with open(filename, "r") as infile:
+        lines = infile.readlines()
+        end_index = min(len(lines), int(end * SAMPLES_PER_SECOND))
+
+        for i in range(start_index, end_index):  # reads in the four waves
+            values = lines[i].strip().split(",")
+            wave1 = np.append(wave1, float(values[0]))
+            wave2 = np.append(wave2, float(values[1]))
+            wave3 = np.append(wave3, float(values[2]))
+            wave4 = np.append(wave4, float(values[3]))
+
+    return (wave1, wave2, wave3, wave4)
+
+
+def process_waveform():
+    wave1, wave2, wave3, wave4 = read("./EEG Data/sample1.csv", 3, 4)
+
+    plt.plot(wave1)
+    plt.show()
+
+    fft1, freq1 = FFT_Filter(wave1)
+    fft2, freq2 = FFT_Filter(wave1)
+    fft3, freq3 = FFT_Filter(wave1)
+    fft4, freq4 = FFT_Filter(wave1)
+
+    plt.plot(freq1, np.sqrt(fft1.real ** 2 + fft1.imag ** 2))
+    plt.show()
+
+    filt_wave1 = abs(np.fft.ifft(fft1))
+    filt_wave2 = abs(np.fft.ifft(fft2))
+    filt_wave3 = abs(np.fft.ifft(fft3))
+    filt_wave4 = abs(np.fft.ifft(fft4))
+
+    plt.plot(filt_wave1)
+    plt.show()
+
+    return (filt_wave1, filt_wave2, filt_wave3, filt_wave4)
+
+
+if __name__ == "__main__":
+    process_waveform()
