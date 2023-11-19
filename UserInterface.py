d589fbfb3437efb93ed35dd2947f27f7578bd42b
Khushdeep1337 <150091893+Khushdeep1337@users.noreply.github.com>
2023-11-18 19:29:45 -0700

    Fixed UIPACKAGE

--- UserInterface.py
+++ UserInterface.py
@@ -1,12 +1,21 @@
-
+import numpy as np
 import tkinter as tk
 import pygame
+import time
+import synthesizer
+
+track = False
+flag = False
 
 playing = False
-
+keyspressedArray = []
 pygame.mixer.init()
 
-pygame.mixer.music.load('UIpackage/Audio1.wav')
+
+if(track == False):
+    pygame.mixer.music.load('Audio1.wav')
+else:
+    pygame.mixer.music.load('Audio2.wav')
 root = tk.Tk()
 
 
@@ -19,25 +28,43 @@ label = tk.Label(root, text="Brainify", font= ('Comic Sans MS', 36), fg='white',
 def playandPause():
     global playing
     if playing:
+        #play(True)
         pygame.mixer.music.pause()
         playButton.config(text="PLAY",command=playandPause)
     else:
+        #play(False)
         pygame.mixer.music.play()
         playButton.config(text="PAUSE",command=playandPause)
 
     playing = not playing
     
-# def play():
-#     music.play()
+# def play(flag):
+#     while not flag:
+#         startTime = time.time()
+#         synthesizer.main('Audio1.wav', keyspressedArray)
+#         synthesizer.main('Audio2.wav', keyspressedArray)
+#         endTime = time.time()
+#         if endTime - startTime > 20:
+#             time.sleep(20-endTime+startTime)
 
-def stop():
+        
+
+#def stop()````
+    #global playing
+    #pygame.mixer.music.stop()
+    #playing = False
+    #playButton.config(text="PLAY",command=playandPause)
+
+
+def status():
     global playing
-    pygame.mixer.music.stop()
-    playing = False
+    if pygame.mixer.music.get_busy() == False:
+        playing = False
+        playButton.config(text="PLAY",command=playandPause)
+    root.after(1000, status)
 
-# def pause():
-#     music.pause()
-
+def insertelm(key):
+    keyspressedArray.append(key)
 
 label.pack()
 buttonFrame = tk.Frame(root)
@@ -45,15 +72,26 @@ buttonFrame = tk.Frame(root)
 buttonFrame.pack(side=tk.BOTTOM, pady=40)
 
 playButton = tk.Button(buttonFrame, text = "PLAY", command= playandPause, height = 5, width = 20)
-#pauseButton = tk.Button(buttonFrame, text = "PAUSE", command= pause, height = 5, width = 20)
-stopButton = tk.Button(buttonFrame, text = "STOP", command= stop, height = 5, width = 20)
+#stopButton = tk.Button(buttonFrame, text = "STOP", command= stop, height = 5, width = 20)
 
 
 playButton.pack(side=tk.LEFT, padx=10)  
-#pauseButton.pack(side=tk.LEFT, padx=10)
-stopButton.pack(side=tk.LEFT, padx=10)
+#stopButton.pack(side=tk.LEFT, padx=10)
 
+piano = tk.Frame(root)
+piano.pack(side=tk.BOTTOM, padx=10)
 
+leftText = tk.Label(piano,text="Choose Key",font = ('Comic Sans MS',24))
+leftText.grid(row=0, column=0, columnspan=7)
+
+buttonLabels = ('A','B','C','D','E','F','G','A#/Bb','F#/Gb','C#/Db','G#/Ab')
+
+for i,key in enumerate (buttonLabels):
+    button = tk.Button(piano,text = key,command= lambda key=key: insertelm(key), height=1,width=10)
+    button.grid(row=1, column=i, padx=1, pady=1)
+
+status()
+track = not track
 root.mainloop()
 
 
